47dd954eeedd6e5cf71e915c1208668045f3aeb3
Etido Elijah <elijahetido@gmail.com>
2020-09-30 11:35:30 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/Candy.java
+++ app/src/main/java/com/pluralsight/candycoded/Candy.java
@@ -2,6 +2,47 @@ package com.pluralsight.candycoded;
 
 public class Candy {
     public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String name;
     public String image;
     public String price;
